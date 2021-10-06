6673a8d0dd8dab9e685c3a1cf6b46aaa207430cb
Zakaria <302586168@student.rocmondriaan.nl>
2021-10-06 17:35:43 +0200

    membership section is verbeterd!

--- Templates/defaults/membership.php
+++ Templates/defaults/membership.php
@@ -2,23 +2,31 @@
     <div class="membership-text">
     <h1> HEALTHONE <br> <span>LIDMAATSCHAP</span> </h1>
     <br>
-    <p class="../">HealthOne geeft jou de mogelijkgeid om fit te worden zoals jij zelf wil. <br>
+    <p>HealthOne geeft jou de mogelijkgeid om fit te worden zoals jij zelf wil. <br>
          Dat doen we onder andere door het aanbieden van verschillende lidmatschaappen. <br>
           Kies het lidmaatschap dat het beste aansluit bij jouw wensen en doelen</p>
           <br>
     </div>
          <!-- CARDS -->
-    <div class="container membership-cards">
+    <div class=" membership-cards">
         <div class="card1">
             <div class="properties__card">
-                <span class="month">6 month</span>
-                <p class="mb-25">$30/m</p>
-                <div class="single-features">
+                <div class="card-top">
+                   
+                    <img src="/img/dumbbell.png" width="60px"> 
+                        
+                    
+                    <br>
+                    <span class="period">4 WEKEN</span>
+                    <p class="price">$30/m</p>
+                </div>
+                <div class="card-middel">
+                    <div class="single-features">
                     <div class="features-icon">
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Onbeperkte apparaten </p>
                     </div>
                 </div>
                 <div class="single-features">
@@ -26,7 +34,7 @@
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Persoonlijke trainer </p>
                     </div>
                 </div>
                 <div class="single-features">
@@ -34,7 +42,7 @@
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Maandelijk opzegbaar </p>
                     </div>
                 </div>
                 <div class="single-features">
@@ -42,24 +50,34 @@
                         <img src="/img/tick.png" width="20px" alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Betaal geen inschrijfgeld </p>
                     </div>
                 </div>
-                <a href="#"> <button class="ju-btn" >WORD LID!</button> </a>
+                </div>
+                <div class="memb-btn">
+                    <a href="#"> <button class="ju-btn" >WORD LID!</button> </a>
+                </div>
+                
             </div> 
         </div>
         
 
+
     	<div class="card2">
             <div class="properties__card">
-                <span class="month">6 month</span>
-                <p class="mb-25">$30/m</p>
+
+                <div class="card-top">
+                <img src="/img/dumbbell2.png" width="105px"> <br>
+                    <span class="period">1 JAAR</span>
+                    <p class="price">$30/m</p>
+                </div>
+                <div class="card-middel">
                 <div class="single-features">
                     <div class="features-icon">
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Onbeperkte apparaten </p>
                     </div>
                 </div>
                 <div class="single-features">
@@ -67,7 +85,7 @@
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Persoonlijke trainer </p>
                     </div>
                 </div>
                 <div class="single-features">
@@ -75,17 +93,21 @@
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Gratis cadeautje! </p>
                     </div>
                 </div><div class="single-features">
                     <div class="features-icon">
                         <img src="/img/tick.png" width="20px"  alt="">
                     </div>
                     <div class="features-caption">
-                        <p>Free riding </p>
+                        <p>Betaal geen inschrijfgeld </p>
                     </div>
                 </div>
-                <a href="#"> <button class="ju-btn" >WORD LID!</button> </a>
+                </div>
+                
+                <div class="memb-btn">
+                    <a href="#"> <button class="ju-btn" >WORD LID!</button> </a>
+                </div>
             </div>
         </div>
 
