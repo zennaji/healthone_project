9947276d6cc6545201eb6872c19f0a86e187a2b9
Outmane Bouhaddouch <Outmane-Bou>
2021-10-06 14:37:02 +0200

    jumbotron aangepast

--- Templates/defaults/footer.php
+++ Templates/defaults/footer.php
@@ -1,4 +1,4 @@
 <footer >
-    <h1 style="background-color: red; color:seashell"> HALLO </h1>
+    
     <p class="clearfix pb-3  text-muted text-center">&copy;Afdeling software development Tinwerf 10, 2544 ED Den Haag.</p>
 </footer>
--- Templates/defaults/pictures.php
+++ Templates/defaults/pictures.php
@@ -3,11 +3,13 @@
 </figure>
  -->
  <div class="jumbotron-hero jumbotron jumbotron-fluid">
-  <div class="con1 ontainer">
+  <div class="con1 ">
       <div class="hero-container">
-      <h1 class="hero-txt1">HI THIS IS </h1>
-      <h1 class="hero-txt2">HEALTH <span class="one-txt" >ONE</span> </h1>
-      <button class="wl-btn" >WORD LID!</button>
+        <h1 class="hero-txt1">HI THIS IS </h1>
+        <h1 class="hero-txt2">HEALTH 
+            <span class="one-txt" >ONE</span> 
+        </h1>
+        <button class="wl-btn" >WORD LID!</button>
       </div>
   </div>
 </div>
