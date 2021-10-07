47b8ecc7f07bf84bd6b818dc38d5a5a1e0bd1351
Zakaria <302586168@student.rocmondriaan.nl>
2021-10-07 10:01:46 +0200

    info section is aangemaakt

--- Templates/defaults/info.php
+++ Templates/defaults/info.php
@@ -1,17 +1,26 @@
 <div class="info-container">
     <div class="location">
-        <div class="info-icon"></div>
-        <h2>Locatie</h2>
-        <p>Tinwerf10,2544 ED Denhaag.</p>
+        <div class="info-icon"> <img src="/img/location.png" width="60px" > </div>
+        <div class="info-txt">
+            <h1>Locatie</h1>
+            <p>Tinwerf10,2544 ED Denhaag.</p>
+        </div>
+        
     </div>
     <div class="phone">
-        <div class="info-icon"></div>
-        <h2>Phone</h2>
-        <p>0705412863</p>
+        <div class="info-icon"><img src="/img/telephone.png" width="60px" >  </div>
+        <div class="info-txt">
+            <h1>Phone</h1>
+            <p>0705412863</p>
+        </div>
+        
     </div>
-    <div class="Email">
-        <div class="info-icon"></div>
-        <h2>Email</h2>
-        <p>healthone@service.nl</p>
+    <div class="email">
+        <div class="info-icon"> <img src="/img/email.png" width="60px" > </div>
+        <div class="info-txt">
+            <h1>Email</h1>
+            <p>healthone@service.nl</p>
+        </div>
+        
     </div>
 </div>
