d11627f6f6696198b5381af96c05c7039a466903
Zakaria <302586168@student.rocmondriaan.nl>
2021-10-07 13:31:29 +0200

    social media section is added

--- Templates/home.php
+++ Templates/home.php
@@ -16,19 +16,16 @@
             include_once ('defaults/reviews.php');
             include_once ('defaults/info.php');
             include_once ('defaults/socialm.php');
-            
-
-            ?>
-
-            <h4>Sportcenter HealthOne</h4>
-            Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
-            Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
-            Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
-            <hr>
-            <?php
             include_once ('defaults/footer.php');
             
+
             ?>
+
+            
+            
+            
+            
+            
         </div>
             </div> 
         
