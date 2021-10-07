d7741e9535a34644ec7dafd3561a7daf9fdce3fd
Zakaria <302586168@student.rocmondriaan.nl>
2021-10-07 21:15:09 +0200

    animation added

---
+++ Templates/defaults/animate.php
@@ -0,0 +1,16 @@
+<div class="div_animate">
+    <img src="/img/animate.png" width="50%">
+</div>
+
+<div class="animated-txt">
+    <div class="child-div">
+        <div class="sign ">
+        <span class="fast-flicker">HE</span>ALT<span class="flicker">H</span>ONE
+        </div>
+        <div class="animate-foto">
+            <img src="/img/girl.png" width="250px">
+
+        </div>
+    </div>
+
+</div>
--- Templates/defaults/pictures.php
+++ Templates/defaults/pictures.php
@@ -6,11 +6,12 @@
   <div class="con1 ">
       <div class="hero-container">
         <h1 class="hero-txt1">HI THIS IS </h1>
-        <h1 class="hero-txt2">HEALTH 
+        <h1 class=" hero-txt2">HEALTH 
             <span class="one-txt" >ONE</span> 
         </h1>
         <button class="wl-btn" >WORD LID!</button>
       </div>
+      
   </div>
 </div>
 
