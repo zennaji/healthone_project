2297807f5643a9048a1c885098ea044f29d4b5c6
Zakaria <302586168@student.rocmondriaan.nl>
2021-10-08 15:35:38 +0200

    section animate1s place is changed

--- Templates/home.php
+++ Templates/home.php
@@ -12,10 +12,10 @@
             include_once ('defaults/menu.php');
             include_once ('defaults/pictures.php');
             include_once ('defaults/equipment.php');
+            include_once ('defaults/animate.php');
             include_once ('defaults/membership.php');
             include_once ('defaults/animate1.php');
             include_once ('defaults/reviews.php');
-            include_once ('defaults/animate.php');
             include_once ('defaults/info.php');
             include_once ('defaults/socialm.php');
             include_once ('defaults/footer.php');
