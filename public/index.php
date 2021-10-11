b450b260dd120cc94c0398af24a015c98fc4636e
Alaa Alkatlabe <302232189@student.rocmondriaan.nl>
2021-10-11 09:20:03 +0200

    contact toegevoegd

--- public/index.php
+++ public/index.php
@@ -37,6 +37,10 @@ switch ($params[1]) {
             include_once "../Templates/categories.php";
         }
         break;
+    case 'contact':
+        $titleSuffix = ' | Contant';
+        include_once "../Templates/contact.php";
+        break;
     default:
         $titleSuffix = ' | Home';
         include_once "../Templates/home.php";
