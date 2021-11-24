300a286c8166d2965e66dae7027cabbcff60b73b
Alaa Alkatlabe <302232189@student.rocmondriaan.nl>
2021-11-24 12:11:21 +0100

    name fixed

--- Templates/access.php
+++ Templates/access.php
@@ -3,6 +3,6 @@ $admin = isset($_SESSION["myrole"]) && $_SESSION["myrole"] == "admin";
 $user = isset($_SESSION["myrole"]) && $_SESSION["myrole"] == "user";
 $guest = isset($_SESSION["myrole"]) && $_SESSION["myrole"] == "guest";
 
-var_dump($admin);
+//var_dump($admin);
 
 ?>
